eb94d86e8586e69424f5c8b72c1dbcec04aacb00
JasonYoo1 <jyoojs@gmail.com>
2019-08-22 15:37:49 -0700

    was working on old movie assignment, transferred to new repo

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -42,6 +42,12 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <button className="save-button"
+        onClick={() => this.props.history.push(`/update-movie/${this.movie.id}`)}
+        className="md-button"
+      >
+        Edit Item
+      </button>
       </div>
     );
   }
